049a011f3f1376dd2fb66d70459fcecbe049fe4e
SaitDev <SaitDev@users.noreply.github.com>
2018-01-19 13:41:52 +0700

    ping command

---
+++ commands/system/ping.js
@@ -0,0 +1,26 @@
+const Command = require('../Command.js')
+
+const info = {
+  name: "ping",
+  aliases: [],
+  description: 'Pong! Check bot\'s response',
+  runIn: ["text", "dm"],
+  ownerOnly: false
+}
+
+
+class Ping extends Command {
+  constructor(client) {
+    super(client, info);
+  }
+
+  execute(msg, args) {
+    if (!this.checkChannel(msg.channel.type)) return;
+
+    if (Math.random() < 0.3) msg.channel.send('OK, worked!')
+    else if (Math.random() < 0.6) msg.channel.send('Yep, Im working!')
+    else msg.channel.send('Pong!');
+  }
+}
+
+module.exports = Ping;
