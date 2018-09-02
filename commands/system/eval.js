34f32941843d5aca54c4dc8c9390710680562ca4
SaitDev <SaitDev@users.noreply.github.com>
2018-09-03 02:11:43 +0700

    eval command
    be careful while playing with it!

---
+++ commands/system/eval.js
@@ -0,0 +1,29 @@
+const Command = require('../Command');
+
+const info = {
+    name: "eval",
+    aliases: ["sudo"],
+    description: "Im curious!!",
+    runIn: ["text", "dm"],
+    ownerOnly: true
+}
+
+class Eval extends Command {
+    constructor(client, moduleName) {
+		super(client, info, moduleName);
+	}
+
+    /**
+     * @param {import('discord.js').Message} msg 
+     * @param {string} args 
+     */
+    run(msg, args) {
+        try {
+            eval(args);
+        } catch (err) {
+            msg.channel.send(`:skull_crossbones: \`${err.name}\` ${err.message}`);
+        }
+    }
+}
+
+module.exports = Eval;
